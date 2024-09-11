470e0a69e8981bf26e3a0fc15d0b5467ba913504
sofiankirat <www.sofyankirat@gmail.com>
2024-09-11 07:20:59 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (Success)
+ */
+int main(void)
+{
+	int alpha = 122;
+
+	while (alpha >= 97)
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
