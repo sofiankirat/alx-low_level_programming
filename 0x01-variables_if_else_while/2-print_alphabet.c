cc30f1d1ad231f55c9cb303b534bad236d369dfa
sofiankirat <www.sofyankirat@gmail.com>
2024-09-11 04:01:37 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alpha = 97;
+
+	while (alpha <= 122)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	printf("\n");
+	return (0);
+}
+
+
