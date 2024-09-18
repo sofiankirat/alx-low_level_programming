8813b18315c082174f9a8efbbb0526f8e1a00995
sofiankirat <www.sofyankirat@gmail.com>
2024-09-17 20:01:48 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,34 @@
  */
 void fibonacci_bignum(void)
 {
-	unsigned long int i = 1, j = 1, z = 1;
+	unsigned long int i = 1, j = 1, z = 1, fir_part, sec_part, thi_part, fou_part;
+	unsigned long int f, s, p, u, k;
 
-	for (; i <= 98; i++)
+	for (; i <= 97; i++)
 	{
-		printf("%lu", j);
-		if (i != 98)
-			printf(", ");
-		j = j + z;
-		z = j - z;
-	}
-	printf("\n");
+		if (j >= 12200160415121876738LU)
+		{
+			if (j == 12200160415121876738LU)
+			{printf("%lu, ", j);
+				fir_part = j % 10000000000, sec_part = (j - fir_part) / 10000000000;
+			thi_part = z % 10000000000, fou_part = (z - thi_part) / 10000000000, j += 1;
+			} s = fir_part + thi_part, f = sec_part + fou_part, thi_part = fir_part;
+			fou_part = sec_part, fir_part = s, sec_part = f;
+			if (f > 10000000000 || s > 10000000000)
+			{k = (f % 10) + ((s - (s % 10000000000)) / 10000000000);
+			if (k <= 9)
+				printf("%lu%lu%lu, ", ((f - (f % 10)) / 10), k, s % 10000000000);
+			else
+			{p = (k - (k % 10)) / 10, k %= 10, u = (((f % 100) - (f % 10)) / 10) + p;
+			printf("%lu%lu%lu", (f - (f % 100)) / 100, u, k);
+			printf("%lu", s % 10000000000);
+				if (i != 97)
+					printf(", ");
+			}} else
+				printf("%lu%lu, ", f, s);
+		} else
+		{printf("%lu, ", j), j = j + z, z = j - z;
+		}} printf("\n");
 }
 
 /**
