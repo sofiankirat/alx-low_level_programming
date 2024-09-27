b2dbf2806deb83b2eac41ceaeffd2c12a156568f
sofiankirat <www.sofyankirat@gmail.com>
2024-09-26 17:12:57 -0700

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,5 +2,16 @@
 
 /**
  * _puts - prints a string, followed by a new line, to stdout
- * @s: 
+ * @str: A pointer to a string whose will print
  *
+ * Return: This function returns no value (void)
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
